101cb6f2ccb0a57a02afe81813931657f6036a53
Alessandro Paucar <apaucare27@gmail.com>
2023-11-11 01:37:08 +0100

    Se han creado las clases, se han relacionado y se ha estructurado el main

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,47 @@
 public class Customer {
+    private String id;
+    private String name;
+    private String surname;
+    private String homeAdress;
+    private Sale sale;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getHomeAdress() {
+        return homeAdress;
+    }
+
+    public void setHomeAdress(String homeAdress) {
+        this.homeAdress = homeAdress;
+    }
+
+    public Sale getSale() {
+        return sale;
+    }
+
+    public void setSale(Sale sale) {
+        this.sale = sale;
+    }
 }
--- src/Sale.java
+++ src/Sale.java
@@ -1,2 +1,58 @@
+import java.util.ArrayList;
+
 public class Sale {
+    private String code;
+    private String saleDate;
+    private String paymentType;
+    private int salePrice;
+    private Customer customer;
+    private ArrayList<Vehicle> vehicles;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getSaleDate() {
+        return saleDate;
+    }
+
+    public void setSaleDate(String saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public int getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(int salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(ArrayList<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
 }
